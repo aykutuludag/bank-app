6efc7731f7ada5a225de238777128f1cc356e1cb
Aykut <huludag@fdnsoft.com>
2025-06-14 04:00:23 +0300

    first phase completed

--- accounts/urls.py
+++ accounts/urls.py
@@ -14,10 +14,19 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import (
+    bank_accounts_list,
+    account_list_page,
+    transaction_list_page,
+    group_list_page,
+    user_list_page
+)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/', include('accounts.urls')),
+    path('accounts/', bank_accounts_list, name='bank_accounts_list'),
+    path('accounts/ui/', account_list_page, name='account_list_page'),
+    path('transactions/ui/', transaction_list_page, name='transaction_list_page'),
+    path('groups/ui/', group_list_page, name='group_list_page'),       # <--- Burada
+    path('users/ui/', user_list_page, name='user_list_page'),          # <--- Burada
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 from django.http import JsonResponse
-from .models import BankAccount
+from .models import BankAccount, Transaction
+from django.contrib.auth.models import User, Group
 
 def bank_accounts_list(request):
     accounts = BankAccount.objects.all()
@@ -15,3 +16,22 @@ def bank_accounts_list(request):
             'balance': float(acc.balance),
         })
     return JsonResponse(data, safe=False)
+
+
+def account_list_page(request):
+    accounts = BankAccount.objects.all()
+    return render(request, 'accounts/account_list.html', {'accounts': accounts})
+
+
+def transaction_list_page(request):
+    transactions = Transaction.objects.select_related('account__user').all()
+    return render(request, 'accounts/transaction_list.html', {'transactions': transactions})
+
+def group_list_page(request):
+    groups = Group.objects.prefetch_related('members').all()
+    return render(request, 'accounts/group_list.html', {'groups': groups})
+
+
+def user_list_page(request):
+    users = User.objects.all()
+    return render(request, 'accounts/user_list.html', {'users': users})
